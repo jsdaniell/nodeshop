6bd51c6e6d77e1b7e94880dc0f89fd33b76ae0a4
danisboy <jose.daniell@outlook.com>
2019-06-09 12:36:40 -0300

    All files added

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// Capturing any request that not have a defined response
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(port);
